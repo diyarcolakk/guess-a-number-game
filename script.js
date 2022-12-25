e10eda3bc6c57a62ba7f459ca8f59e4e9f08b788
diyarcolakk <114312886+diyarcolakk@users.noreply.github.com>
2022-12-26 02:04:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// console.log(document.querySelector('.message').textContent)
+
+// document.querySelector('.message').textContent = 'Doğru sayı'
+
+// console.log(document.querySelector('.message').textContent)
+
+// document.querySelector('.number').textContent = 15
+// // console.log(document.querySelector('.number'))
+// document.querySelector('.score').textContent = 10
+
+// console.log(document.querySelector('.guess').value)
+
+// document.querySelector('.guess').value = 25
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value)
+})
